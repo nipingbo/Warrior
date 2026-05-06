9bef89aec05c034bbe902f878fa1b1ebe3fbbe66
Raymond Ni <nipingbo@gmail.com>
2026-05-06 14:48:06 +0800

    Native Gameplay Tags

---
+++ Source/Warrior/Public/WarriorGameplayTags.h
@@ -0,0 +1,16 @@
+// Raymond Study
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "NativeGameplayTags.h"
+/**
+ * 
+ */
+namespace WarriorGameplayTags
+{
+	/* Input Tags */
+	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	WARRIOR_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+};
