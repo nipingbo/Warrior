599b5ff551676f48b354e4dff3c46ced83af5099
Raymond Ni <nipingbo@gmail.com>
2026-05-08 20:16:03 +0800

    Hero Anim Instance

--- Source/Warrior/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
+++ Source/Warrior/Public/AnimInstances/Hero/WarriorHeroAnimInstance.h
@@ -6,6 +6,7 @@
 #include "AnimInstances/WarriorCharacterAnimInstance.h"
 #include "WarriorHeroAnimInstance.generated.h"
 
+class AWarriorHeroCharacter;
 /**
  * 
  */
@@ -14,4 +15,19 @@ class WARRIOR_API UWarriorHeroAnimInstance : public UWarriorCharacterAnimInstanc
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+	
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Warrior|AnimData|Reference")
+	AWarriorHeroCharacter* OwningHeroCharacter;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Warrior|AnimData|Locomotion")
+	bool bShouldEnterRelaxState;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Warrior|AnimData|Locomotion")
+	float EnterRelaxStateThreshold = 5.f;
+	
+	float IdleElapsedTime;
 };
