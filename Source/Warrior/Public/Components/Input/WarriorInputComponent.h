79fc755fab8506052822e48b6e3ac59e2b8bff48
Raymond Ni <nipingbo@gmail.com>
2026-05-08 09:56:06 +0800

    Custom Enhanced Input Component

---
+++ Source/Warrior/Public/Components/Input/WarriorInputComponent.h
@@ -0,0 +1,32 @@
+// Raymond Study
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+#include "WarriorInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UWarriorInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+	
+public:
+	template<class UserObject, typename CallbackFunc>
+	void BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func);
+};
+
+template <class UserObject, typename CallbackFunc>
+void UWarriorInputComponent::BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig,
+	const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func)
+{
+	checkf(InInputConfig, TEXT("Invalid InputConfig for UWarriorInputComponent."));
+	if ( UInputAction* FoundAction = InInputConfig->FindNativeInputActionByTag(InInputTag))
+	{
+		BindAction(FoundAction, TriggerEvent, ContextObject, Func);
+	}
+}
