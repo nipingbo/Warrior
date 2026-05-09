e8937526403e2304ff14068e1b422f55d5edcdea
Raymond Ni <nipingbo@gmail.com>
2026-05-09 20:08:53 +0800

    Hero Gameplay Ability

---
+++ Source/Warrior/Private/AbilitySystem/Abilities/WarriorHeroGameplayAbility.cpp
@@ -0,0 +1,30 @@
+// Raymond Study
+
+
+#include "AbilitySystem/Abilities/WarriorHeroGameplayAbility.h"
+
+#include "Characters/WarriorHeroCharacter.h"
+#include "Controllers/WarriorHeroController.h"
+
+AWarriorHeroCharacter* UWarriorHeroGameplayAbility::GetHeroCharacterFromActorInfo()
+{
+	if (!CachedWarriorHeroCharacter.IsValid())
+	{
+		CachedWarriorHeroCharacter = Cast<AWarriorHeroCharacter>(CurrentActorInfo->AvatarActor);
+	}
+	return CachedWarriorHeroCharacter.IsValid()? CachedWarriorHeroCharacter.Get(): nullptr;
+}
+
+AWarriorHeroController* UWarriorHeroGameplayAbility::GetHeroControllerFromActorInfo()
+{
+	if (!CachedWarriorHeroController.IsValid())
+	{
+		CachedWarriorHeroController = Cast<AWarriorHeroController>(CurrentActorInfo->PlayerController);
+	}
+	return CachedWarriorHeroController.IsValid()? CachedWarriorHeroController.Get(): nullptr;
+}
+
+UHeroCombatComponent* UWarriorHeroGameplayAbility::GetHeroCombatComponentFromActorInfo()
+{
+	return GetHeroCharacterFromActorInfo()->GetHeroCombatComponent();
+}
