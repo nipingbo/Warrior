b6ddaff85aa221aef3ed4c89f5d5462f74610016
Raymond Ni <nipingbo@gmail.com>
2026-05-25 17:01:21 +0800

    Init Attribute Set

--- Source/Warrior/Public/AbilitySystem/WarriorAttributeSet.h
+++ Source/Warrior/Public/AbilitySystem/WarriorAttributeSet.h
@@ -3,8 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystem/WarriorAbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "WarriorAttributeSet.generated.h"
+
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -14,4 +21,30 @@ class WARRIOR_API UWarriorAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UWarriorAttributeSet();
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Warrior|Health")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, CurrentHealth);
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Warrior|Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, MaxHealth);
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Warrior|Rage")
+	FGameplayAttributeData CurrentRage;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, CurrentRage);
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Warrior|Rage")
+	FGameplayAttributeData MaxRage;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, MaxRage);
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Warrior|Damage")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, AttackPower);
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Warrior|Damage")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(UWarriorAttributeSet, DefensePower);
 };
